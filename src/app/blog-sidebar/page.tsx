ee0268b672fb4b025fa46f41def038922e1def90
Arunava Ghosh <arunavaghsh84@gmail.com>
2024-11-06 00:32:53 +0530

    Fixed wrong import

--- src/app/blog-sidebar/page.tsx
+++ src/app/blog-sidebar/page.tsx
@@ -1,7 +1,7 @@
 import RelatedPost from "@/components/Blog/RelatedPost";
 import SharePost from "@/components/Blog/SharePost";
 import TagButton from "@/components/Blog/TagButton";
-import NewsLatterBox from "@/components/Contact/GetDocumentBox";
+import NewsLatterBox from "@/components/Products/GetDocumentBox";
 import Image from "next/image";
 
 import { Metadata } from "next";
