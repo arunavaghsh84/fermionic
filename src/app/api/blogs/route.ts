782e4770c37ba7163ba80ccb0e7df59350a84084
Arunava Ghosh <arunavaghsh84@gmail.com>
2024-11-07 22:25:55 +0530

    Send error message instead of error object

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -33,6 +33,6 @@ export async function GET() {
 
     return NextResponse.json(blogs, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
