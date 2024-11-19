1eff1955f9f4e8378365be3fa5d9794149262d99
Prosanta Karmakar <karmakar.prosanta@gmail.com>
2024-11-19 17:55:04 +0530

    header menu update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import ThemeToggler from "./ThemeToggler";
-import menuData from "./menuData";
+// import ThemeToggler from "./ThemeToggler";
+// import menuData from "./menuData";
 
 const Header = () => {
   // Navbar toggle
   const [navbarOpen, setNavbarOpen] = useState(false);
-  const [menuItems, setMenuItems] = useState(menuData);
+  const [menuItems, setMenuItems] = useState();
 
   const navbarToggleHandler = () => {
     setNavbarOpen(!navbarOpen);
@@ -40,54 +40,54 @@ const Header = () => {
 
   const usePathName = usePathname();
 
-  useEffect(() => {
-    fetchProducts();
-    fetchSiliconIPs();
-  }, []);
+  // useEffect(() => {
+  //   fetchProducts();
+  //   fetchSiliconIPs();
+  // }, []);
 
-  const fetchProducts = async () => {
-    const res = await fetch("/api/products");
+  // const fetchProducts = async () => {
+  //   const res = await fetch("/api/products");
 
-    if (res.ok) {
-      const data = await res.json();
+  //   if (res.ok) {
+  //     const data = await res.json();
 
-      const menu = menuItems.find((item) => item.id === 2);
-      menu.submenu = data.map(({ _id, name }) => {
-        return {
-          id: _id,
-          title: name,
-          path: `/products/${_id}`,
-          newTab: false,
-        };
-      });
+  //     const menu = menuItems.find((item) => item.id === 2);
+  //     menu.submenu = data.map(({ _id, name }) => {
+  //       return {
+  //         id: _id,
+  //         title: name,
+  //         path: `/products/${_id}`,
+  //         newTab: false,
+  //       };
+  //     });
 
-      setMenuItems(menuItems);
-    } else {
-      console.error("Error fetching products");
-    }
-  };
+  //     setMenuItems(menuItems);
+  //   } else {
+  //     console.error("Error fetching products");
+  //   }
+  // };
 
-  const fetchSiliconIPs = async () => {
-    const res = await fetch("/api/siliconIPs");
+  // const fetchSiliconIPs = async () => {
+  //   const res = await fetch("/api/siliconIPs");
 
-    if (res.ok) {
-      const data = await res.json();
+  //   if (res.ok) {
+  //     const data = await res.json();
 
-      const menu = menuItems.find((item) => item.id === 3);
-      menu.submenu = data.map(({ _id, name }) => {
-        return {
-          id: _id,
-          title: name,
-          path: `/siliconIPs/${_id}`,
-          newTab: false,
-        };
-      });
+  //     const menu = menuItems.find((item) => item.id === 3);
+  //     menu.submenu = data.map(({ _id, name }) => {
+  //       return {
+  //         id: _id,
+  //         title: name,
+  //         path: `/siliconIPs/${_id}`,
+  //         newTab: false,
+  //       };
+  //     });
 
-      setMenuItems(menuItems);
-    } else {
-      console.error("Error fetching products");
-    }
-  };
+  //     setMenuItems(menuItems);
+  //   } else {
+  //     console.error("Error fetching products");
+  //   }
+  // };
 
   return (
     <>
@@ -149,76 +149,135 @@ const Header = () => {
                   }`}
                 >
                   <ul className="block lg:flex lg:space-x-8">
-                    {menuData.map((menuItem, index) => (
-                      <li key={index} className="group relative">
-                        {menuItem.path ? (
-                          <Link
-                            href={menuItem.path}
-                            className={`flex py-2 text-sm lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${
-                              usePathName === menuItem.path
-                                ? "text-primary dark:text-white"
-                                : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
-                            }`}
-                          >
-                            {menuItem.title}
-                          </Link>
-                        ) : (
-                          <>
-                            <p
-                              onClick={() => handleSubmenu(index)}
-                              className="flex cursor-pointer items-center justify-between py-2 text-sm text-dark group-hover:text-primary dark:text-white/70 dark:group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
-                            >
-                              {menuItem.title}
-                              <span className="pl-3">
-                                <svg width="25" height="24" viewBox="0 0 25 24">
-                                  <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
-                                    fill="currentColor"
-                                  />
-                                </svg>
-                              </span>
-                            </p>
-                            <div
-                              className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
-                                openIndex === index ? "block" : "hidden"
-                              }`}
-                            >
-                              {menuItem.submenu.map((submenuItem, index) => (
-                                <Link
-                                  href={submenuItem.path}
-                                  key={index}
-                                  className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3"
-                                >
-                                  {submenuItem.title}
-                                </Link>
-                              ))}
-                            </div>
-                          </>
-                        )}
-                      </li>
-                    ))}
+                    <li className="group relative">
+                      <Link
+                        href="/"
+                        className={`flex py-2 text-sm lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${
+                          usePathName === "/" ? "text-primary dark:text-white" : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
+                        }`}
+                      >
+                        Home
+                      </Link>
+                    </li>
+
+                    <li className="group relative">
+                      <p
+                        onClick={() => handleSubmenu(0)} // Adjust if needed for toggling
+                        className="flex cursor-pointer items-center justify-between py-2 text-sm text-dark group-hover:text-primary dark:text-white/70 dark:group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
+                      >
+                        Product
+                        <span className="pl-3">
+                          <svg width="25" height="24" viewBox="0 0 25 24">
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </span>
+                      </p>
+                      <div className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full hidden`}>
+                        <Link href="/product-details" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          RF Beamformers
+                        </Link>
+                        <Link href="/product-details" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          RF Front End Modules
+                        </Link>
+                        <Link href="/product-details" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          Power Management
+                        </Link>
+                        <Link href="/product-details" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          Microcontrollers
+                        </Link>
+                      </div>
+                    </li>
+
+                    <li className="group relative">
+                      <p
+                        onClick={() => handleSubmenu(0)} // Adjust if needed for toggling
+                        className="flex cursor-pointer items-center justify-between py-2 text-sm text-dark group-hover:text-primary dark:text-white/70 dark:group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
+                      >
+                        Silicon IP
+                        <span className="pl-3">
+                          <svg width="25" height="24" viewBox="0 0 25 24">
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </span>
+                      </p>
+                      <div className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full hidden`}>
+                        <Link href="/product-details" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          Multi-Protocol SERDES
+                        </Link>
+                        <Link href="/product-details" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          Phase Locked Loop
+                        </Link>
+                        <Link href="/product-details" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          Miscellaneous
+                        </Link>
+                      </div>
+                    </li>
+
+                    <li className="group relative">
+                      <p
+                        onClick={() => handleSubmenu(0)} // Adjust if needed for toggling
+                        className="flex cursor-pointer items-center justify-between py-2 text-sm text-dark group-hover:text-primary dark:text-white/70 dark:group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
+                      >
+                        Company
+                        <span className="pl-3">
+                          <svg width="25" height="24" viewBox="0 0 25 24">
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </span>
+                      </p>
+                      <div className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full hidden`}>
+                        <Link href="/about" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          About Us
+                        </Link>
+                        <Link href="/leadership" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          Leadership
+                        </Link>
+                        <Link href="/career" className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3">
+                          Career
+                        </Link>
+                      </div>
+                    </li>
+                    
+                    <li className="group relative">
+                      <Link
+                        href="/blogs"
+                        className={`flex py-2 text-sm lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${
+                          usePathName === "/blogs" ? "text-primary dark:text-white" : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
+                        }`}
+                      >
+                        Blogs
+                      </Link>
+                    </li>
+
+                    <li className="group relative">
+                      <Link
+                        href="/contact"
+                        className={`flex py-2 text-sm lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${
+                          usePathName === "/contact" ? "text-primary dark:text-white" : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
+                        }`}
+                      >
+                        Contact Us
+                      </Link>
+                    </li>
                   </ul>
+
                 </nav>
               </div>
-              {/* <div className="flex items-center justify-end pr-16 lg:pr-0">
-                <Link
-                  href="/signin"
-                  className="hidden px-7 py-3 text-sm font-medium text-dark hover:opacity-70 dark:text-white md:block"
-                >
-                  Sign In
-                </Link>
-                <Link
-                  href="/signup"
-                  className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-primary px-8 py-3 text-sm font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9"
-                >
-                  Sign Up
-                </Link>
-                <div>
-                  <ThemeToggler />
-                </div>
-              </div> */}
             </div>
           </div>
         </div>
