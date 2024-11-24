0df87744fdc7bc9bdcf225cb59b69e9be4535907
Arunava Ghosh <arunavaghsh84@gmail.com>
2024-11-25 01:33:52 +0530

    Update TinyMCE API key

--- src/components/Common/TinyEditor/TinyEditor.tsx
+++ src/components/Common/TinyEditor/TinyEditor.tsx
@@ -1,29 +1,32 @@
 // components/TinyEditor.tsx
-import React from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 interface TinyEditorProps {
   initialValue?: string;
   onEditorChange: (content: string) => void;
 }
 
-const TinyEditor: React.FC<TinyEditorProps> = ({ initialValue = '', onEditorChange }) => {
+const TinyEditor: React.FC<TinyEditorProps> = ({
+  initialValue = "",
+  onEditorChange,
+}) => {
   return (
     <Editor
-      apiKey="36dd2vw35ylnzdin3xn5cev2ie0v0xn1thybnqf9bsr306p1" // Replace with your TinyMCE API key
-      initialValue={initialValue}  // Set initial HTML content here
+      apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY} // Replace with your TinyMCE API key
+      initialValue={initialValue} // Set initial HTML content here
       init={{
         height: 500,
         menubar: false,
         plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount',
+          "advlist autolink lists link image charmap print preview anchor",
+          "searchreplace visualblocks code fullscreen",
+          "insertdatetime media table paste code help wordcount",
         ],
         toolbar:
-          'undo redo | formatselect | bold italic backcolor | \
+          "undo redo | formatselect | bold italic backcolor | \
           alignleft aligncenter alignright alignjustify | \
-          bullist numlist outdent indent | removeformat | help',
+          bullist numlist outdent indent | removeformat | help",
       }}
       onEditorChange={onEditorChange}
     />
