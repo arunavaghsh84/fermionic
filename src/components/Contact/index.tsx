34e34c898fe0eaf7439b51a044f99e7c227a327d
Prosanta Karmakar <karmakar.prosanta@gmail.com>
2024-12-06 15:31:51 +0530

    Google map fixed

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -196,7 +196,7 @@ const Contact = () => {
                         name="message"
                         value={formData.message}
                         onChange={handleChange}
-                        rows={2}
+                        rows={5}
                         placeholder="Enter your Message"
                         className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       ></textarea>
